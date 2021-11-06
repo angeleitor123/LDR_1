75eaea2bf812d9b6f86e4e8f280b828160dec733
angeleitor123 <65198882+angeleitor123@users.noreply.github.com>
2021-11-06 17:43:21 -0600

    Add files via upload

---
+++ sensor de luz_2/src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+/*
+const long A = 1000;     //Resistencia en oscuridad en KΩ
+const int B = 15;        //Resistencia a la luz (10 Lux) en KΩ
+const int Rc = 1;       //Resistencia calibracion en KΩ
+const int LDRPin = 35;   //Pin del LDR
+int V;
+int ilum;
+void setup() 
+{
+   Serial.begin(115200);
+}
+void loop()
+{
+   V = analogRead(LDRPin);         
+   //ilum = ((long)(1024-V)*A*10)/((long)B*Rc*V);  //usar si LDR entre GND y A0 
+   ilum = ((long)V*A*10)/((long)B*Rc*(1024-V));    //usar si LDR entre A0 y Vcc (como en el esquema anterior)
+  
+   Serial.println(ilum);   
+   delay(1000);
+}
+*/
+int luzsensor =35; // puerto 25 despinado a la lectura de luz 
+                    
+int luz = 0;  // variable de lectura 
+
+void setup() {
+  Serial.begin(115200);           //  setup serial
+}
+
+void loop() {
+  luz = analogRead(luzsensor);  // read the input pin
+  Serial.println(luz);          // debug value
+  delay(250);
+}
