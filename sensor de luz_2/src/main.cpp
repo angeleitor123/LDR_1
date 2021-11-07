7d692fbe9a083b047edfbeadfd0e44628b2fde73
angeleitor123 <65198882+angeleitor123@users.noreply.github.com>
2021-11-06 22:17:30 -0600

    Update main.cpp

--- sensor de luz_2/src/main.cpp
+++ sensor de luz_2/src/main.cpp
@@ -6,23 +6,23 @@ const int Rc = 1;       //Resistencia calibracion en KΩ
 const int LDRPin = 35;   //Pin del LDR
 int V;
 int ilum;
-void setup() 
+void setup()
 {
    Serial.begin(115200);
 }
 void loop()
 {
-   V = analogRead(LDRPin);         
-   //ilum = ((long)(1024-V)*A*10)/((long)B*Rc*V);  //usar si LDR entre GND y A0 
+   V = analogRead(LDRPin);
+   //ilum = ((long)(1024-V)*A*10)/((long)B*Rc*V);  //usar si LDR entre GND y A0
    ilum = ((long)V*A*10)/((long)B*Rc*(1024-V));    //usar si LDR entre A0 y Vcc (como en el esquema anterior)
-  
-   Serial.println(ilum);   
+
+   Serial.println(ilum);
    delay(1000);
 }
 */
-int luzsensor =35; // puerto 25 despinado a la lectura de luz 
-                    
-int luz = 0;  // variable de lectura 
+int luzsensor = 35; // puerto 25 despinado a la lectura de luz 
+
+int luz = 0;  // variable de lectura
 
 void setup() {
   Serial.begin(115200);           //  setup serial
